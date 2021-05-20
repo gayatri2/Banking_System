4ae7a1bc4de6c0d5790593d1dd998789b36945dc
gayatri2 <gayatri.godbole02@gmail.com>
2021-05-20 18:23:52 +0530

    All pages along with UI,  completed.

--- my-app/src/components/Transfers.jsx
+++ my-app/src/components/Transfers.jsx
@@ -1,13 +1,125 @@
-import React from "react";
+import React, { useState } from "react";
+import Axios from "axios";
+import gif from "../images/1uoA-unscreen.gif";
 
 
 
 function Transfers() {
+  const [toname, settoname]=useState('');
+  const [fromname, setfromname]=useState('');
+  const [amount,setamount]=useState('');
+
+  const transfers = function(e){
+    e.preventDefault();
+    console.log(toname, fromname, amount);
+    var x=document.getElementById("div1");
+    x.style.display="none";
+
+    var b=document.getElementById("div4");
+      b.style.display="block";
+    var link = "http://localhost:3001/transact/"+toname+"/"+fromname+"/"+amount ;
+    console.log(link);
+    Axios.get(link).then((response)=>{
+      console.log(response);
+      function disap()
+      {
+        b.style.display="none";
+        var flag=response.data.flag;
+        if(flag==1)
+        {
+          var s=document.getElementById("div2");
+          s.style.display="block";
+        }
+        else{
+          var f=document.getElementById("div3");
+          f.style.display="block";
+        }
+        
+
+      }
+      setTimeout(disap,4000);
+     
+    })
+   }
+
+   
+
     return (
       <div>
-        <h1>Transfers' page.</h1>
         
+       
+
+          <form  id="div1">
+            
+            <div class="form-row a">
+              <div class="form-group col-md-6 sm-12">
+                  <h1 class="t">Transfers</h1>
+                  <label for="formGroupExampleInput">Customer 1 (TO):</label>
+                  <input type="text" name="toname" class="form-control" id="formGroupExampleInput" placeholder="Customer1"  onChange={function(e){ 
+                    settoname(e.target.value);  //to get the value that is entered in the front end.
+                    }}/>
+            
+              </div>
+              </div>
+              <div class="row a">
+            
+            <div class="form-group col-md-6 sm-12">
+              <label for="formGroupExampleInput2">Customer 2 (FROM):</label>
+              <input type="text" name="fromname"class="form-control" id="formGroupExampleInput2" placeholder="Customer2" onChange={function(e){ 
+              setfromname(e.target.value);  //to get the value that is entered in the front end.
+              }}/>
+            </div>
+        </div>
+          <div class="row a">
+            <div class="form-group col-md-6 sm-12">
+              <label for="formGroupExampleInput">Amount to be transferred:</label>
+              <input type="text" name="amount"class="form-control amount" id="formGroupExampleInput" placeholder="Amount" onChange={function(e){ 
+        setamount(e.target.value);  //to get the value that is entered in the front end.
+      }}/>
+            </div>
+            </div>
+            <button type="submit" onClick={transfers} class="btn btn-info bb">Transfer</button>
+            
+          </form>
+        
+          <div id="div2">
+            <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700&display=swap" rel="stylesheet" />
+            <script src="https://kit.fontawesome.com/c8ee3dd930.js"></script>
+
+            <div class="container">
+              <div class="card ts">
+                <div class="title">
+     
+                  <h2><b>"Transaction successful"</b></h2>
+                </div>
+                
+                <div class="circle"></div>
+              </div>
+            </div>
+
+          </div>
+  
+  
+          <div id="div3">
+            <div class="container">
+              <div class="card tf">
+                <div class="title">
+     
+                  <h2><b>"Transaction failed"</b></h2>
+                </div>
+                
+                <div class="circle"></div>
+              </div>
+            </div>
+          
+
+          </div>
+          <div id="div4"><img src={gif} class="giff" /></div>
+
+
       </div>
+          
+      
     );
   }
   export default Transfers;
--- server/index.js
+++ server/index.js
@@ -81,6 +81,92 @@ app.get("/Customerinfo/:inp",function(req,res){
     
 
 })
+app.get("/transact/:p1/:p2/:p3",function(req,res){
+    var prm1=req.params.p1;
+    var prm2=req.params.p2;
+    var prm3=req.params.p3;
+
+    console.log(prm1);
+    console.log(prm2);
+    console.log(prm3);
+
+    global.flag=0;  //send in the form of an object or string, since node cant send number
+    global.bal_1=0;
+    global.bal_2=0;
+
+
+    const sqlSearch_1="SELECT * FROM customers WHERE customers.name=(?)";
+    db.query(sqlSearch_1,[prm1],function(err,response){
+      if(err)
+      {
+          console.log(err);
+      }
+      else
+      {
+          //console.log("Entry saved into DB"); 
+          console.log(response);
+          bal_1=response[0].balance;
+          //console.log(bal_1);
+          const sqlSearch_2="SELECT * FROM customers WHERE customers.name=(?)";
+          db.query(sqlSearch_2,[prm2],function(err,response){
+            if(err)
+            {
+                console.log(err);
+            }
+            else
+            {
+                //console.log("Entry saved into DB"); 
+                console.log(response);
+                bal_2=response[0].balance;
+                bal_1=Number(bal_1)+Number(prm3);
+                bal_2=Number(bal_2)-Number(prm3);
+                console.log(bal_1,bal_2);
+                if(Number(bal_2)<0)
+                {
+                  console.log("insuff");
+                  res.send({"flag":flag});
+                }
+                
+                else{
+
+                console.log(bal_1,bal_2);
+                flag=1;
+                console.log("success");
+                const sqlUpdate_1 = "UPDATE customers SET balance=(?) WHERE name=(?)"
+                db.query(sqlUpdate_1,[bal_1,prm1],function(err,reply){
+                  if(err)
+                  {
+                      console.log(err);
+                  }
+                  else
+                  {
+                    console.log(reply);
+                    res.send({"flag":flag});
+                  }
+                });
+                const sqlUpdate_2 = "UPDATE customers SET balance=(?) WHERE name=(?)"
+                db.query(sqlUpdate_2,[bal_2,prm2],function(err,reply){
+                  if(err)
+                  {
+                      console.log(err);
+                  }
+                  else
+                  {
+                    console.log(reply);
+                  }
+                });
+              }
+
+            }
+          });
+        
+
+      }
+  });
+  
+    
+
+})
 
 
 
